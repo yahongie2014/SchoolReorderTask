849a1d0c16967a484b5d6c6429b53ef694d9c051
Ahmed Saeed <a.saeed@null.net>
2022-03-22 03:13:02 +0200

    validate readme

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', [\App\Http\Controllers\HomeController::class, 'CreateOrLogin']);
 
 Route::middleware(['auth:api','AdminAuth'])->group(function () {
-
     Route::apiResource('Student', 'App\Http\Controllers\StudentController');
     Route::apiResource('School', 'App\Http\Controllers\SchoolController');
 });
